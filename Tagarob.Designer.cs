f8d1ac7aaf7af36b72e19c40294c480284b1d248
Mohamed Fouad <Pronaggar@outlook.com>
2023-01-11 03:07:20 +0200

    \

---
+++ Tagarob.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Tagarob
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.picDataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            this.patientsDBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.formBBDataSet = new WindowsFormsApp3.FormBBDataSet();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.patientsDTableAdapter = new WindowsFormsApp3.FormBBDataSetTableAdapters.patientsDTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patientsDBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.formBBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.picDataGridViewImageColumn});
+            this.dataGridView1.DataSource = this.patientsDBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 24);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1660, 243);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            this.idDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 10;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 200;
+            // 
+            // picDataGridViewImageColumn
+            // 
+            this.picDataGridViewImageColumn.DataPropertyName = "pic";
+            this.picDataGridViewImageColumn.HeaderText = "pic";
+            this.picDataGridViewImageColumn.MinimumWidth = 10;
+            this.picDataGridViewImageColumn.Name = "picDataGridViewImageColumn";
+            this.picDataGridViewImageColumn.Width = 200;
+            // 
+            // patientsDBindingSource
+            // 
+            this.patientsDBindingSource.DataMember = "patientsD";
+            this.patientsDBindingSource.DataSource = this.formBBDataSet;
+            // 
+            // formBBDataSet
+            // 
+            this.formBBDataSet.DataSetName = "FormBBDataSet";
+            this.formBBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(206, 273);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 82;
+            this.dataGridView2.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // patientsDTableAdapter
+            // 
+            this.patientsDTableAdapter.ClearBeforeFill = true;
+            // 
+            // Tagarob
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1707, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Tagarob";
+            this.Text = "Tagarob";
+            this.Load += new System.EventHandler(this.Tagarob_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.patientsDBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.formBBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private FormBBDataSet formBBDataSet;
+        private System.Windows.Forms.BindingSource patientsDBindingSource;
+        private FormBBDataSetTableAdapters.patientsDTableAdapter patientsDTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn picDataGridViewImageColumn;
+    }
+}
