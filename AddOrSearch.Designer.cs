11fb653c4e53025204e4d813caa45da377953aa4
AceyPe <Yahya.Adawy50@gmail.com>
2023-01-11 03:15:38 +0200

    nameSpace Fixed

--- AddOrSearch.Designer.cs
+++ AddOrSearch.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp3
+﻿namespace ClinicApplication
 {
     partial class AddOrSearch
     {
--- AddOrSearch.cs
+++ AddOrSearch.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormsApp3
+namespace ClinicApplication
 {
     public partial class AddOrSearch : Form
     {
